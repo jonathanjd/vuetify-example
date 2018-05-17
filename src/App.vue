5c100be52f3d2168a4a460810a2063d81a63d878
Jonathan Duran <headjd@gmail.com>
2018-05-17 14:34:43 -0400

    Text-Alignment

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <v-app>
-      <app-power-vuetify></app-power-vuetify>
+      <app-text-alignment></app-text-alignment>
     </v-app>
 </template>
 
@@ -11,6 +11,7 @@ import Order from './components/Order';
 import DirectionAlign from './components/DirectionAlign';
 import RowColumnBreakPoint from './components/RowColumnBreakPoint';
 import PowerVuetify from './components/PowerVuetify';
+import TextAlignment from './components/TextAlignment';
 export default {
   components: {
     appGridSystem: GridSystem,
@@ -18,7 +19,8 @@ export default {
     appOrder: Order,
     appDirectionAlign: DirectionAlign,
     appRowColumnBreakPoint: RowColumnBreakPoint,
-    appPowerVuetify: PowerVuetify
+    appPowerVuetify: PowerVuetify,
+    appTextAlignment: TextAlignment
   }
 };
 </script>
---
+++ src/components/TextAlignment.vue
@@ -0,0 +1,22 @@
+<template>
+<v-container grid-list-xs>
+  <v-layout row wrap>
+    <v-flex xs12>
+      <v-card class="mt-5 pa-5">
+        <v-card-text>
+          <p class="text-xs-left">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p class="text-xs-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p class="text-xs-right">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
